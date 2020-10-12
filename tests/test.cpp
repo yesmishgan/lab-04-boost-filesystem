f962dbb4f0f764c5bd522dc8148cbd6fb43aa0da
mishadolbnin <dolbnin@protonmail.com>
2020-10-13 00:53:27 +0300

    Добавление тестов

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,3 +13,16 @@ TEST(dirParse, testFuncPrint){
       A.printAccount(A.getPathToAccount(0), 1).str(),
       "broker: ip       account: 00001234 files: 1   lastdate: 20181001");
 }
+
+TEST(dirParse, testOverload){
+  std::ostringstream output;
+  dirParse A("/home/travis/build/yesmishgan/lab-04-boost-filesystem//ftp");
+  output << A;
+  EXPECT_EQ(output, R"(
+broker: ip       account: 00001234 files: 1   lastdate: 20181001
+broker: ip       account: 00001346 files: 2   lastdate: 20181018
+broker: ip       account: 00003333 files: 1   lastdate: 20181001
+broker: ip.dos   account: 00003335 files: 1   lastdate: 20181001
+broker: ip.dos   account: 00003336 files: 1   lastdate: 20181001
+broker: bcs      account: 12341234 files: 1   lastdate: 20181001)");
+}
