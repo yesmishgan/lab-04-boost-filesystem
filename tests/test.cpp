71388c947b13b499bf89b91bd4054d56411a7768
mishadolbnin <dolbnin@protonmail.com>
2020-10-13 11:20:06 +0300

    Добавление тестов

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,8 +18,9 @@ TEST(dirParse, testOverload){
   std::ostringstream output;
   dirParse A("/home/travis/build/yesmishgan/lab-04-boost-filesystem//ftp");
   output << A;
-  EXPECT_EQ(output.str(), R"(
-broker: ip       account: 00001234 files: 1   lastdate: 20181001
+  EXPECT_EQ(
+      output.str(),
+      R"(broker: ip       account: 00001234 files: 1   lastdate: 20181001
 broker: ip       account: 00001346 files: 2   lastdate: 20181018
 broker: ip       account: 00003333 files: 1   lastdate: 20181001
 broker: ip.dos   account: 00003335 files: 1   lastdate: 20181001
